edee0352db98f6f059bc2c83aed711933dfd77e6
Anonymouse Baby <babyanonymouse2@gmail.com>
2024-06-03 17:44:54 +0300

    responsive js fix

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ window.onscroll = () => {
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach.apply((links) => {
+      navLinks.forEach((links) => {
         links.classList.remove("active");
         document
           .querySelector("header nav a[href*=" + id + "]")
