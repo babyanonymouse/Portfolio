35971265120ba20c90b6514392131d9f09e867cb
babyanonymouse <babyanonymouse2@gmail.com>
2025-01-28 14:17:30 +0300

    responsive design start

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { Link as ScrollLink } from "react-scroll";
+import { HiOutlineMenuAlt3, HiX } from "react-icons/hi";
 
 // navbar json
 const navLinks = [
@@ -10,20 +12,37 @@ const navLinks = [
 ];
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="text-white bg-bodydark">
-      <nav className="fixed top-0 left-0 w-full p-3 shadow-md shadow-white z-20 lg:relative lg:shadow-none max-w-[110rem] mx-auto">
+      <nav className="fixed top-0 left-0 w-full p-3 shadow-md z-20 lg:relative lg:shadow-none max-w-[110rem] mx-auto">
         <motion.div
           className="flex justify-between items-center p-4 lg:px-20"
           initial={{ y: -250 }}
           animate={{ y: 0 }}
           transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
         >
-          {/* logo */}
+          {/* Logo */}
           <div className="font-bebas text-4xl hover:text-light duration-150">
             <Link href="/">Samuel Lwanga</Link>
           </div>
-          {/* links */}
+
+          {/* Hamburger Menu for Mobile */}
+          <div className="lg:hidden">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-3xl text-light focus:outline-none"
+            >
+              {isMenuOpen ? <HiX /> : <HiOutlineMenuAlt3 />}
+            </button>
+          </div>
+
+          {/* Desktop Navigation */}
           <div className="hidden lg:block font-inter cursor-pointer">
             <ul className="flex space-x-6 text-xl text-light">
               {navLinks.map((link) => (
@@ -36,6 +55,34 @@ const Header = () => {
             </ul>
           </div>
         </motion.div>
+
+        {/* Mobile Navigation */}
+        {isMenuOpen && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="lg:hidden bg-bodydark absolute top-full left-0 w-full shadow-md"
+          >
+            <ul className="flex flex-col items-center space-y-4 p-4 text-xl text-light font-inter">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className="hover:text-white duration-200"
+                  onClick={closeMenu} // Close menu when clicked
+                >
+                  <ScrollLink
+                    to={link.link}
+                    smooth={true}
+                    duration={500}
+                    onClick={closeMenu} // Close menu on link click
+                  >
+                    {link.title}
+                  </ScrollLink>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
       </nav>
     </header>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head />
       <title>Samuel Lwanga</title>
-      <body className="antialiased bg-bodydark text-white">
+      <body className="antialiased bg-bodydark text-white font-manrope">
         <Main />
         <NextScript />
       </body>
