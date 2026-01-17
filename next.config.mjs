33a086e53e96df7218ebec3ceb64039fe9392caf
Anonymouse Baby <babyanonymouse2@gmail.com>
2026-01-17 20:40:28 +0300

    refactor: Clean up next.config.mjs by removing commented code and unused imports.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
-import next from "next";
-import path from "path";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
